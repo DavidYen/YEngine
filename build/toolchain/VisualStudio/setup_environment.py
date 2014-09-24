ff223cafa95f9296ba8020af029cef5a201356fe
David Yen <nokkie@gmail.com>
2014-09-23 20:21:20 -0700

    Initial GN Implementation

---
+++ build/toolchain/VisualStudio/setup_environment.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+SCRIPT_DIR = os.path.dirname(__file__)
+
+ENVIRON32_FILE = os.path.join(SCRIPT_DIR, 'environment.x86')
+ENVIRON64_FILE = os.path.join(SCRIPT_DIR, 'environment.x64')
+
+def WriteWindowEnvFile(env_dict, env_file):
+  with open(env_file, 'wb') as f:
+    for key, value in env_dict.iteritems():
+      f.write(key + '=' + value + '\0')
+    f.write('\0')
+
+def DoMain(argv):
+  if len(argv) != 2 or '--h' in argv or '--help' in argv or '\?' in argv:
+    print 'Usage: %s vs_name vs_path' % sys.argv[0]
+    return 1
+
+  vs_name, vs_path = argv
+  if vs_name == 'SDK':
+    path32 = [os.path.join(vs_path, 'bin', 'x86')]
+    path64 = [os.path.join(vs_path, 'bin', 'x64')]
+  elif vs_name.startswith('VS'):
+    path32 = [os.path.join(vs_path, 'VC', 'BIN')]
+    path64 = [os.path.join(vs_path, 'VC', 'BIN', 'amd64')]
+
+  env_dict32 = os.environ.copy()
+  env_dict64 = os.environ.copy()
+
+  env_dict32['PATH'] = ';'.join(env_dict32['PATH'].split(';') + path32)
+  env_dict64['PATH'] = ';'.join(env_dict64['PATH'].split(';') + path64)
+
+  WriteWindowEnvFile(env_dict32, ENVIRON32_FILE)
+  WriteWindowEnvFile(env_dict64, ENVIRON64_FILE)
+  return 0
+
+if __name__ == "__main__":
+  try:
+    sys.exit(DoMain(sys.argv[1:]))
+  except Exception as e:
+    print str(e)
+    print 1
---
+++ build/visual_studio_paths.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python
+
+import argparse
+import json
+import os
+import sys
+import traceback
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+DEFAULT_CONFIG_FILE = os.path.join(SCRIPT_DIR, 'VSConfig.json')
+DEFAULT_CONFIG_FILENAME = os.path.basename(DEFAULT_CONFIG_FILE)
+
+def GetConfigDict(args):
+  config_json = {}
+  if os.path.isfile(args.config_file):
+    try:
+      with open(args.config_file, 'rt') as f:
+        config_json = json.load(f)
+    except Exception as e:
+      print 'Could not load file:', args.config_file
+      raise e
+
+  return config_json
+
+def SaveConfigDict(args, config_dict):
+  with open(args.config_file, 'wt') as f:
+    json.dump(config_dict, f, sort_keys=True, indent=2)
+
+def do_config(args):
+  config_dict = GetConfigDict(args)
+  config_dict[args.vs_name] = args.vs_path
+
+  SaveConfigDict(args, config_dict)
+  return 0
+
+def do_list(args):
+  config_json = GetConfigDict(args)
+  if not config_json:
+    print 'No Visual Studio paths found'
+    return 1
+
+  for name, path in config_json.iter_items():
+    print '%s: %s' % (name, path)
+
+  return 0
+
+def do_output(args):
+  name = args.output_name
+  config_dict = GetConfigDict(args)
+  if name not in config_dict:
+    print 'Visual Studio Path is not set:', name
+    print 'Configure Usage: %s config %s vs_path' % (sys.argv[0], name)
+    return 1
+
+  print config_dict[name]
+  return 0
+
+def do_main(argv):
+  parser = argparse.ArgumentParser(prog=sys.argv[0])
+
+  command_parser = parser.add_subparsers(title='command', dest='command')
+  parser.add_argument(
+      '--config-file', dest='config_file', action='store',
+      default=DEFAULT_CONFIG_FILE,
+      help='VS Configuration file. (default: %s)' % DEFAULT_CONFIG_FILENAME)
+
+  # Configure Command
+  config_parser = command_parser.add_parser('config')
+  config_parser.add_argument(
+      'vs_name',
+      help='Visual Studio Name: IE. VS20XX, SDK, VS20XXe...etc.')
+  config_parser.add_argument(
+      'vs_path',
+      help='Path for the Visual Studio path')
+
+  # List Command
+  config_parser = command_parser.add_parser('list')
+
+  # Output Visual Studio Command
+  output_parser = command_parser.add_parser('output')
+  output_parser.add_argument(
+      '--name', dest='output_name', action='store',
+      help='Output the path for a given visual studio name.')
+
+  args = parser.parse_args(argv)
+
+  if (args.command == 'config'):
+    return do_config(args)
+  elif (args.command == 'list'):
+    return do_list(args)
+  elif (args.command == 'output'):
+    return do_output(args)
+
+  print 'Invalid Command:', args.command
+  return 1
+
+if __name__ == "__main__":
+  try:
+    sys.exit(do_main(sys.argv[1:]))
+  except Exception as e:
+    traceback.print_exc()
+    sys.exit(1)
