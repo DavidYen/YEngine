e78eb95692d431fa20470a4ce61facf7edf0582f
David Yen <david.yen@gmail.com>
2015-07-08 21:59:43 -0700

    Fixed string table initialization.

--- YEngine/YCore/StringTable.cpp
+++ YEngine/YCore/StringTable.cpp
@@ -7,7 +7,7 @@ namespace YEngine { namespace YCore {
 
 namespace {
   YCommon::YContainers::AtomicHashTable gHashTable;
-  size_t max_string_size = 0;
+  size_t gMaxStringSize = 0;
 }
 
 size_t GetAllocationSize(size_t max_string_size, size_t table_size) {
@@ -17,20 +17,22 @@ size_t GetAllocationSize(size_t max_string_size, size_t table_size) {
 
 void StringTable::Initialize(size_t max_string_size, size_t table_size,
                              void* buffer, size_t buffer_size) {
+  YDEBUG_CHECK(max_string_size > 0 && table_size > 0,
+               "Invalid StringTable initialization values.");
   gHashTable.Init(buffer, buffer_size, table_size, max_string_size);
-  max_string_size = max_string_size;
+  gMaxStringSize = max_string_size;
 }
 
 void StringTable::Terminate() {
   gHashTable.Reset();
-  max_string_size = 0;
+  gMaxStringSize = 0;
 }
 
 uint64_t StringTable::AddString(const char* string, size_t string_size) {
-  YDEBUG_CHECK(string_size <= max_string_size,
+  YDEBUG_CHECK(string_size <= gMaxStringSize,
                "Cannot exceed maximum string size: %u > %u",
                static_cast<uint32_t>(string_size),
-               static_cast<uint32_t>(max_string_size));
+               static_cast<uint32_t>(gMaxStringSize));
   uint64_t hash_key = 0;
   gHashTable.Insert(string, string_size, string, string_size, &hash_key);
   return hash_key;
