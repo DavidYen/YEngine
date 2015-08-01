d9747405c427dd8847ea6fa5b07d4b2918cc3213
David Yen <david.yen@gmail.com>
2015-07-31 21:50:22 -0700

    Added Unordered Array

---
+++ YCommon/YContainers/UnorderedArray.cpp
@@ -0,0 +1,87 @@
+#include <YCommon/Headers/stdincludes.h>
+#include "UnorderedArray.h"
+
+#include <string>
+
+namespace YCommon { namespace YContainers {
+
+UnorderedArray::UnorderedArray()
+  : mBuffer(nullptr),
+    mItemSize(0),
+    mItemCount(0),
+    mTotalSize(0),
+    mSwappedItemCallback(nullptr),
+    mCallbackArg(nullptr) {
+}
+
+UnorderedArray::UnorderedArray(void* buffer, size_t buffer_size,
+                               size_t item_size, uint32_t num_items)
+  : mBuffer(nullptr),
+    mItemSize(0),
+    mItemCount(0),
+    mTotalSize(0),
+    mSwappedItemCallback(nullptr),
+    mCallbackArg(nullptr) {
+  Init(buffer, buffer_size, item_size, num_items);
+}
+
+UnorderedArray::~UnorderedArray() {
+}
+
+void UnorderedArray::Init(void* buffer, size_t buffer_size,
+                          size_t item_size, uint32_t num_items) {
+  YASSERT(reinterpret_cast<uintptr_t>(buffer) % item_size == 0,
+          "Unordered Array must be aligned to %u bytes - supplied %p.",
+          item_size, buffer);
+  YASSERT(item_size * num_items <= buffer_size,
+          "Unordered Array requires %u bytes, supplied %u bytes.",
+          buffer_size, item_size * num_items);
+
+  mBuffer = buffer;
+  mItemSize = item_size;
+  mItemCount = 0;
+  mTotalSize = num_items;
+  mSwappedItemCallback = nullptr;
+  mCallbackArg = nullptr;
+}
+
+void UnorderedArray::Reset() {
+  mBuffer = nullptr;
+  mItemSize = 0;
+  mItemCount = 0;
+  mTotalSize = 0;
+  mSwappedItemCallback = nullptr;
+  mCallbackArg = nullptr;
+}
+
+void UnorderedArray::Clear() {
+  mItemCount = 0;
+}
+
+void UnorderedArray::SetItemSwappedCallBack(ItemSwapped callback_routine,
+                                            void* arg) {
+  mSwappedItemCallback = callback_routine;
+  mCallbackArg = arg;
+}
+
+// Allocates or Removes an index
+uint32_t UnorderedArray::Allocate() {
+  if (mItemCount < mTotalSize) {
+    return mItemCount++;
+  }
+  return static_cast<uint32_t>(-1);
+}
+
+void UnorderedArray::Remove(uint32_t index) {
+  YASSERT(index < mItemCount,
+          "Invalid index to remove from array[%u]: %u", mItemCount, index);
+  if (index != mItemCount - 1) {
+    if (mSwappedItemCallback) {
+      mSwappedItemCallback(mItemCount - 1, index, mCallbackArg);
+    }
+    memcpy(GetData(index), GetData(mItemCount-1), mItemSize);
+  }
+  mItemCount--;
+}
+
+}} // namespace YCommon { namespace YContainers {
---
+++ YCommon/YContainers/UnorderedArray.h
@@ -0,0 +1,99 @@
+#ifndef YCOMMON_YCONTAINERS_UNORDEREDARRAY_H
+#define YCOMMON_YCONTAINERS_UNORDEREDARRAY_H
+
+/*******
+* Array where index is not kept constant useful for keeping track of
+*   items in an contiguous array with fast insertion/deletions.
+********/
+namespace YCommon { namespace YContainers {
+
+typedef void (*ItemSwapped)(uint32_t old_index, uint32_t new_index, void* arg);
+
+class UnorderedArray {
+ public:
+  UnorderedArray();
+  UnorderedArray(void* buffer, size_t buffer_size,
+                 size_t item_size, uint32_t num_items);
+  ~UnorderedArray();
+
+  void Init(void* buffer, size_t buffer_size,
+            size_t item_size, uint32_t num_items);
+  void Reset();
+  void Clear();
+
+  void SetItemSwappedCallBack(ItemSwapped callback_routine,
+                              void* arg = nullptr);
+
+  // Allocates or Removes an index
+  uint32_t Allocate(); // returns -1 on failure.
+  void Remove(uint32_t index);
+
+  void* GetData(uint32_t index) {
+    return static_cast<uint8_t*>(mBuffer) + (mItemSize * index);
+  }
+
+  size_t GetItemSize() { return mItemSize; }
+  uint32_t GetCount() { return mItemCount; }
+  uint32_t GetTotalSize() { return mTotalSize; }
+
+ protected:
+  void* mBuffer;
+  size_t mItemSize;
+  uint32_t mItemCount;
+  uint32_t mTotalSize;
+
+  ItemSwapped mSwappedItemCallback;
+  void* mCallbackArg;
+};
+
+template<typename T>
+class TypedUnorderedArray : public UnorderedArray {
+ public:
+  TypedUnorderedArray() : UnorderedArray() {}
+  TypedUnorderedArray(T* buffer, size_t buffer_size, uint32_t num_items)
+      : UnorderedArray(static_cast<void*>(buffer), buffer_size,
+                    sizeof(T), num_items) {}
+  ~TypedUnorderedArray() {}
+
+  void Init(T* buffer, size_t buffer_size, uint32_t num_items) {
+    return UnorderedArray::Init(static_cast<void*>(buffer), buffer_size,
+                             sizeof(T), num_items);
+  }
+
+  uint32_t PushBack(const T* data) {
+    const uint32_t index = Allocate();
+    memcpy(GetData(index), data, sizeof(data));
+    return index;
+  }
+
+  uint32_t PushBack(const T& data) {
+    return PushBack(&data);
+  }
+
+  const T& operator[](size_t index) const {
+    return static_cast<T*>(mBuffer)[index];
+  }
+
+  T& operator[](size_t index) {
+    return static_cast<T*>(mBuffer)[index];
+  }
+};
+
+template<typename T, size_t items>
+class ContainedUnorderedArray : public TypedUnorderedArray<T> {
+ public:
+  ContainedUnorderedArray() : TypedUnorderedArray(mData, sizeof(mData), items) {
+  }
+  ~ContainedUnorderedArray() {}
+
+  void Init() {
+    TypedUnorderedArray::Init(mData, sizeof(mData), items);
+  }
+
+ private:
+  T mData[items];
+};
+
+}} // namespace YCommon { namespace YContainers {
+
+#endif // YCOMMON_YCONTAINERS_UNORDEREDARRAY_H
