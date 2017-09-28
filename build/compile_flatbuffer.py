f2de2469f613fb5211da748ee9e017ce4696b7db
David Yen <nokkie@gmail.com>
2017-09-27 20:34:01 -0700

    Added support for python flatbuffer modules.

--- build/compile_flatbuffer.py
+++ build/compile_flatbuffer.py
@@ -7,8 +7,8 @@ import sys
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument(
-    "--depfile", required=True,
-    help="Where flatbuffer depfile will be written to.")
+    "--depfile",
+    help="Where flatbuffer depfile should be written to.")
   parser.add_argument(
     "flatc_args", nargs="+",
     help="Application arguments.")
@@ -16,8 +16,9 @@ if __name__ == "__main__":
   args = parser.parse_args()
 
   subprocess.check_call(args.flatc_args)
-  deps_output = subprocess.check_output([args.flatc_args[0], "-M"] +
-                                        args.flatc_args[1:])
-  with open(args.depfile, "wb") as f:
-    f.write(deps_output)
+  if args.depfile:
+    deps_output = subprocess.check_output([args.flatc_args[0], "-M"] +
+                                          args.flatc_args[1:])
+    with open(args.depfile, "wb") as f:
+      f.write(deps_output)
   sys.exit(0)
